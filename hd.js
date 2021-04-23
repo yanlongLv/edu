5c63f72489fa6947994f68d395c3720bc30eb582
apple <apple@appledeiMac.lan>
2021-04-23 11:31:20 +0800

    add project

---
+++ hd.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+TextInput = new Array(2);
+TextInput[0]="18629371285";
+TextInput[1]="18629371286";
+TextInput[2]="18629371287";
+index = Math.floor(Math.random() * TextInput.length);
+
+
