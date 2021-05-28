6299c4281da232069a274ea8fe07f76650d0b349
yanlonglv <lyl_office@163.com>
2021-05-28 21:49:02 +0800

    add js code

--- hd.js
+++ hd.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 TextInput = new Array(1);
-TextInput[0]="18629371285";
-TextInput[1]="18629371286";
+TextInput[0]="15709207696";
+TextInput[1]="17782804780";
 index = Math.floor(Math.random() * TextInput.length);
 
 
