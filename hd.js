2645539617e21a3f9630d65e191dfc6ac6e8c42d
yanlonglv <lyl_office@163.com>
2021-05-28 21:03:59 +0800

    add wx picture

--- hd.js
+++ hd.js
@@ -1,8 +1,7 @@
 // JavaScript Document
-TextInput = new Array(2);
+TextInput = new Array(1);
 TextInput[0]="18629371285";
 TextInput[1]="18629371286";
-TextInput[2]="18629371287";
 index = Math.floor(Math.random() * TextInput.length);
 
 
